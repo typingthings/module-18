a2fa6d6fbfc1c50fb102d882d75ec1d13ae5b35c
typingthings <zachboatright123@gmail.com>
2022-10-28 21:01:09 -0500

    new stuff

---
+++ model.js
@@ -0,0 +1,29 @@
+Username 
+    String  
+    Unique 
+    Required
+    Trimmed
+
+Email 
+    String 
+    Required
+    Unique
+    validEmail
+
+Thoughts 
+
+Friends 
+    friendCount Schema 
+
+thoughtText 
+    String - How thoughts form! 
+
+createdAt === date 
+
+username 
+    stringRequired 
+    dateRequired 
+    
+Reactions
+    liveReplies
+    reactionsHere 
